2ca4162a6adea3c2e918f47c17bc9a182ddf7527
Franco Modarelli <francomoda@gmail.com>
2021-05-18 21:17:24 -0300

    Modificaciones Desafios 4 y 5 Greeting incluido en h5

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <ProductContainer stock={5} initial='1'/>
+      <ProductContainer stock={5} initial='1' greeting='Bienvenido'/>
     </div>
   );
 }
--- src/components/Products/ProductContainer.js
+++ src/components/Products/ProductContainer.js
@@ -4,7 +4,7 @@ import './ProductContainer.css'
 import '../Buttons/Buttons.css'
 
 
-const ProductContainer = ({initial, stock}) =>{
+const ProductContainer = ({initial, stock, greeting}) =>{
     
     //Counter para sumar y restar
     const [counter,setCounter] = useState(Number(initial));
@@ -20,7 +20,7 @@ const ProductContainer = ({initial, stock}) =>{
         <div className="card">
             <img src="#" className="card-img-top" alt="Producto Epping Books" />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">{ greeting }</h5>
                 <div className='container'>
                     <button className='btn-- btn--small btn--secondary' onClick={() => changeCounter(-1)} disabled = { counter === 1 ? true : null}><i className='fas fa-minus' /></button>
                     <input type='text' className='counter' value={ counter } readOnly/>
